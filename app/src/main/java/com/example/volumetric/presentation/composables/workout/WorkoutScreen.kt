fc29645d993f74c13c2c9030b93bd0caa79d6774
Ritesh <riteshsingh.py@gmail.com>
2026-03-08 10:50:02 +0530

    Started working on workout screen

---
+++ app/src/main/java/com/example/volumetric/presentation/composables/workout/WorkoutScreen.kt
@@ -0,0 +1,9 @@
+package com.example.volumetric.presentation.composables.workout
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun WorkoutScreen() {
+    Text("This is the Workout Screen")
+}
